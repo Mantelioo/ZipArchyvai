c606f6daf26b8b6a5092e65355572b08a543ea8a
Mantelioo <80548252+Mantelioo@users.noreply.github.com>
2021-05-26 17:27:54 +0300

    Add project files.

---
+++ ZipArchyvai/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ZipArchyvai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtPath = new System.Windows.Forms.TextBox();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LstContents = new System.Windows.Forms.ListBox();
+            this.BtnZipFromFolder = new System.Windows.Forms.Button();
+            this.ExtractZipToFolder = new System.Windows.Forms.Button();
+            this.Extract7z = new System.Windows.Forms.Button();
+            this.Create7z = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Archive";
+            // 
+            // TxtPath
+            // 
+            this.TxtPath.Location = new System.Drawing.Point(133, 11);
+            this.TxtPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtPath.Name = "TxtPath";
+            this.TxtPath.ReadOnly = true;
+            this.TxtPath.Size = new System.Drawing.Size(325, 22);
+            this.TxtPath.TabIndex = 11;
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(481, 11);
+            this.BtnBrowse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(100, 33);
+            this.BtnBrowse.TabIndex = 12;
+            this.BtnBrowse.Text = "...";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Archive contents";
+            // 
+            // LstContents
+            // 
+            this.LstContents.FormattingEnabled = true;
+            this.LstContents.ItemHeight = 16;
+            this.LstContents.Location = new System.Drawing.Point(24, 81);
+            this.LstContents.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LstContents.Name = "LstContents";
+            this.LstContents.Size = new System.Drawing.Size(675, 484);
+            this.LstContents.TabIndex = 14;
+            // 
+            // BtnZipFromFolder
+            // 
+            this.BtnZipFromFolder.Location = new System.Drawing.Point(24, 579);
+            this.BtnZipFromFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnZipFromFolder.Name = "BtnZipFromFolder";
+            this.BtnZipFromFolder.Size = new System.Drawing.Size(140, 44);
+            this.BtnZipFromFolder.TabIndex = 15;
+            this.BtnZipFromFolder.Text = "Create zip from folder";
+            this.BtnZipFromFolder.UseVisualStyleBackColor = true;
+            this.BtnZipFromFolder.Click += new System.EventHandler(this.BtnZipFromFolder_Click);
+            // 
+            // ExtractZipToFolder
+            // 
+            this.ExtractZipToFolder.Location = new System.Drawing.Point(172, 579);
+            this.ExtractZipToFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ExtractZipToFolder.Name = "ExtractZipToFolder";
+            this.ExtractZipToFolder.Size = new System.Drawing.Size(140, 44);
+            this.ExtractZipToFolder.TabIndex = 16;
+            this.ExtractZipToFolder.Text = "Extract zip to folder";
+            this.ExtractZipToFolder.UseVisualStyleBackColor = true;
+            this.ExtractZipToFolder.Click += new System.EventHandler(this.ExtractZipToFolder_Click);
+            // 
+            // Extract7z
+            // 
+            this.Extract7z.Location = new System.Drawing.Point(560, 579);
+            this.Extract7z.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Extract7z.Name = "Extract7z";
+            this.Extract7z.Size = new System.Drawing.Size(140, 44);
+            this.Extract7z.TabIndex = 17;
+            this.Extract7z.Text = "Extract zip to folder 7zip";
+            this.Extract7z.UseVisualStyleBackColor = true;
+            this.Extract7z.Click += new System.EventHandler(this.Extract7z_Click);
+            // 
+            // Create7z
+            // 
+            this.Create7z.Location = new System.Drawing.Point(412, 579);
+            this.Create7z.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Create7z.Name = "Create7z";
+            this.Create7z.Size = new System.Drawing.Size(140, 44);
+            this.Create7z.TabIndex = 18;
+            this.Create7z.Text = "Create zip from folder";
+            this.Create7z.UseVisualStyleBackColor = true;
+            this.Create7z.Click += new System.EventHandler(this.Create7z_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(24, 630);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(676, 46);
+            this.progressBar1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 699);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Create7z);
+            this.Controls.Add(this.Extract7z);
+            this.Controls.Add(this.ExtractZipToFolder);
+            this.Controls.Add(this.BtnZipFromFolder);
+            this.Controls.Add(this.LstContents);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnBrowse);
+            this.Controls.Add(this.TxtPath);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtPath;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox LstContents;
+        private System.Windows.Forms.Button BtnZipFromFolder;
+        private System.Windows.Forms.Button ExtractZipToFolder;
+        private System.Windows.Forms.Button Extract7z;
+        private System.Windows.Forms.Button Create7z;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ZipArchyvai/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Papidomos bibliotekos
+using System.IO.Compression;
+using System.IO;
+using SevenZip; //NuGet paketas
+using System.Threading;
+
+
+namespace ZipArchyvai
+{
+    public partial class Form1 : Form
+    {
+        //ZIP kintamieji
+        string arhivePath, source, destination;
+        string extractSource, extractDestination;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            LstContents.Items.Clear();
+
+            var ofd = new OpenFileDialog()
+            {
+                Title = "Select archive",
+                Filter = "Zip archive|*.zip",
+                Multiselect = false,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            };
+
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                TxtPath.Text = ofd.FileName;
+                arhivePath = ofd.FileName;
+                ShowArchive(ofd.FileName); //pasiziurim archivo turini
+            }
+            else
+            {
+                LstContents.Items.Add("No archive selected");
+                TxtPath.Text = string.Empty;
+            }
+        }
+        private void ExtractZipToFolder_Click(object sender, EventArgs e)
+        {
+            //1 pasirenkam .zip archiva
+            var ofd = new OpenFileDialog()
+            {
+                Title = "Select archive",
+                Filter = "Zip archive|*.zip",
+                Multiselect = false,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            };
+
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.OK) //reikai nurodyti i koki kataloga isarchivuot turini
+            {
+                extractSource = ofd.FileName; //source
+
+                //2 Reikia pasirinkti folderi
+                FolderBrowserDialog fbd = new FolderBrowserDialog()
+                {
+                    Description = "Select folder to extract archive",
+                    ShowNewFolderButton = true,
+                    RootFolder = Environment.SpecialFolder.Desktop,
+                };
+
+                DialogResult folderResult = fbd.ShowDialog(); //destination
+
+                if (folderResult == DialogResult.OK)
+                {
+                    extractDestination = fbd.SelectedPath;
+                    ZipFile.ExtractToDirectory(extractSource, extractDestination);
+                }
+            }
+            else
+            {
+                LstContents.Items.Add("No archive selected");
+                TxtPath.Text = string.Empty;
+            }
+
+        }
+        private void Create7z_Click(object sender, EventArgs e) // is katalogo susikuriam archyva
+        {
+            LstContents.Items.Clear();
+
+            //1 Reikia pasirinkti folderi
+            FolderBrowserDialog fbd = new FolderBrowserDialog()
+            {
+                Description = "Select folder for archiving",
+                ShowNewFolderButton = false,
+                RootFolder = Environment.SpecialFolder.Desktop,
+            };
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                source = fbd.SelectedPath; //pasirenkama kataloga
+
+                // 2 Reikia parinkti vieta archyvui saugoti
+                SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Title = "Select where to save archive",
+                    Filter = "Zip file|*.zip"
+                };
+
+                DialogResult safeFileResult = sfd.ShowDialog();
+                if (safeFileResult == DialogResult.OK)
+                {
+                    //Cia naudosim 7zip
+                    destination = sfd.FileName;
+                    SevenZipCompressor.SetLibraryPath(Application.StartupPath + @"\7z.dll");
+                    SevenZipCompressor sevenZcomppr = new SevenZipCompressor();
+                    sevenZcomppr.CompressDirectory(source, destination);
+                }
+                else
+                {
+                    LstContents.Items.Add("No location selected");
+                    TxtPath.Text = string.Empty;
+                }
+            }
+            else
+            {
+                LstContents.Items.Add("No folder selected");
+                TxtPath.Text = string.Empty;
+            }
+        }
+        private void Extract7z_Click(object sender, EventArgs e)
+        {
+            //1 pasirenkam .zip archiva
+            var ofd = new OpenFileDialog()
+            {
+                Title = "Select archive",
+                Filter = "Zip archive|*.zip",
+                Multiselect = false,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            };
+
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.OK) //reikai nurodyti i koki kataloga isarchivuot turini
+            {
+                extractSource = ofd.FileName; //source
+
+                //2 Reikia pasirinkti folderi
+                FolderBrowserDialog fbd = new FolderBrowserDialog()
+                {
+                    Description = "Select folder to extract archive",
+                    ShowNewFolderButton = true,
+                    RootFolder = Environment.SpecialFolder.Desktop,
+                };
+
+                DialogResult folderResult = fbd.ShowDialog(); //destination
+
+                if (folderResult == DialogResult.OK)
+                {
+                    extractDestination = fbd.SelectedPath;
+                    
+                    SevenZipExtractor.SetLibraryPath(Application.StartupPath + @"\7z.dll");
+                    SevenZipExtractor sevenZ = new SevenZipExtractor(extractSource);
+
+                    //SevenZ veikia kaip bacground workeris
+                    sevenZ.Extracting += SevenZ_Extracting;
+                    sevenZ.ExtractionFinished += SevenZ_ExtractionFinished;
+                    sevenZ.FileExists += SevenZ_FileExists;
+
+                    sevenZ.ExtractArchive(extractDestination); //kur sukelti archivo failiukus
+                }
+            }
+            else
+            {
+                LstContents.Items.Add("No archive selected");
+                TxtPath.Text = string.Empty;
+            }
+        }
+        private void SevenZ_FileExists(object sender, FileOverwriteEventArgs e)
+        {
+         
+        }
+        private void SevenZ_ExtractionFinished(object sender, EventArgs e)
+        {
+            MessageBox.Show("Finished");
+            progressBar1.Value = 0;
+        }
+        private void SevenZ_Extracting(object sender, ProgressEventArgs e)
+        {
+            progressBar1.Value = e.PercentDone; //kiek procentu atlikta
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ShowArchive(string path)//Funkcija kuri gauna .zip archivo turini.
+        {
+            try
+            {
+                ZipArchive zip = ZipFile.OpenRead(path); // jei cia nulus
+                foreach (var item in zip.Entries)
+                {
+                    LstContents.Items.Add(item.FullName);
+                }
+            }
+            catch (Exception ex) // cia parodys error
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnZipFromFolder_Click(object sender, EventArgs e)
+        {
+            LstContents.Items.Clear();
+
+            //1 Reikia pasirinkti folderi
+            FolderBrowserDialog fbd = new FolderBrowserDialog()
+            {
+                Description = "Select folder for archiving",
+                ShowNewFolderButton = false,
+                RootFolder = Environment.SpecialFolder.Desktop,
+            };
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                source = fbd.SelectedPath; //pasirenkama kataloga
+
+                // 2 Reikia parinkti vieta archyvui saugoti
+                SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Title = "Select where to save archive",
+                    Filter = "Zip file|*.zip"
+                };
+
+               DialogResult safeFileResult = sfd.ShowDialog();
+                if (safeFileResult == DialogResult.OK)
+                {
+                    destination = sfd.FileName; //Kur saugosim archyva
+
+                    //Archyvo generavimas gali uztrukti. Naudojam thread
+                    Thread th = new Thread(StartZip);
+                    th.Start();
+                }
+                else
+                {
+                    LstContents.Items.Add("No location selected");
+                    TxtPath.Text = string.Empty;
+                }
+            }
+            else
+            {
+                LstContents.Items.Add("No folder selected");
+                TxtPath.Text = string.Empty;
+            }
+
+        }
+        private void StartZip()
+        {
+            //Parenkami archyvavimo parametrai
+            ZipFile.CreateFromDirectory(source, destination, System.IO.Compression.CompressionLevel.Optimal, false);
+            MessageBox.Show("Completed");
+        }
+    }
+}
